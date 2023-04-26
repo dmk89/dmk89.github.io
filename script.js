c45ff22f4c154ab053f88eeeaff5b15e3020fde3
dmk89 <126948531+dmk89@users.noreply.github.com>
2023-04-26 10:44:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-0
+	window.playerLoad = async (src) => {
+		if (window.isMusicActive) {
+			window.music.src = src
+			window.music.load()
+			
+			try {
+				music.title = "Audionautix - " + /[^\/]*$/.exec(src)[0]
+			} catch (err) {
+				window.WKLog(err.message + '\n' + err.stack)
+			}
+			
+			window.WKLoad(src)
+		}
+	}
